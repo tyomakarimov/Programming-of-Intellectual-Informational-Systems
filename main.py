54ef19551b303a42b94c162099d69ab215925e50
tyomakarimov <karartvas170202@gmail.com>
2021-09-07 23:57:00 +0300

    added game loop

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 600
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+  run = True
+  while run:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
+  pygame.quit()
+
+if __name__ == '__main__':
+  main()
