cffaeb43a683114e4acdb34c29c7346898574297
tyomakarimov <karartvas170202@gmail.com>
2021-10-05 23:06:16 +0300

    added DFS algorithm

---
+++ DFS.py
@@ -0,0 +1,25 @@
+import matrix
+from helpers import the_neighbours
+
+def DFS(start, target):
+  current_matrix = matrix.generate_matrix()
+  stack = [start]
+  path = []
+  visited = []
+  while len(stack) > 0:
+    current = stack.pop()
+    visited.append(current)
+    if current == target:
+      break
+    neighbours = the_neighbours.get_neighbours(current_matrix, current)
+    for neighbour in neighbours:
+      if neighbour not in visited:
+        stack.append(neighbour)
+        path.append({ 'current': current, 'next': neighbour })
+  path_to_target = [target]
+  while target != start:
+    for step in path:
+      if step['next'] == target:
+        target = step['current']
+        path_to_target.insert(0, step['current'])
+  return path_to_target
