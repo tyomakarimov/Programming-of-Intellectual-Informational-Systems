ae97614b476e97d69921d7b60f41ec3242bc313e
tyomakarimov <karartvas170202@gmail.com>
2021-10-05 23:05:16 +0300

    added BFS algorithm

---
+++ BFS.py
@@ -0,0 +1,27 @@
+import matrix
+from helpers import the_neighbours
+
+def BFS(start, target):
+  current_matrix = matrix.generate_matrix()
+  queue = [start]
+  path = []
+  visited = []
+  while len(queue) > 0:
+    current = queue[0]
+    queue.remove(queue[0])
+    visited.append(current)
+    if current == target:
+      break
+    else:
+      neighbours = the_neighbours.get_neighbours(current_matrix, current)
+      for neighbour in neighbours:
+        if neighbour not in visited and neighbour not in queue:
+          queue.append(neighbour)
+          path.append({ 'current': current, 'next': neighbour })
+  path_to_target = [target]
+  while target != start:
+    for step in path:
+      if step['next'] == target:
+        target = step['current']
+        path_to_target.insert(0, step['current'])
+  return path_to_target
